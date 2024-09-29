7a6f216b89ea7cc80211fc3f2fb4797cee943785
Alexander Petree <apetree1001@email.phoenix.edu>
2024-09-29 04:03:42 -0700

    Update test-metadata-generation.sh

--- test-metadata-generation.sh
+++ test-metadata-generation.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 
-set -exuo pipefail
-
-source tools.sh
-source createdisk-library.sh
-source snc-library.sh
-
+set -exuo
+pipefail source tools.sh
+source createdisk-library.sh source snc-library.sh
 MIRROR=${MIRROR:-https://mirror.openshift.com/pub/openshift-v4/$ARCH/clients/ocp}
-OPENSHIFT_RELEASE_VERSION="${OPENSHIFT_VERSION-4.7.0}"
-
-SNC_PRODUCT_NAME=${SNC_PRODUCT_NAME:-crc}
+OPENSHIFT_RELEASE_VERSION="${OPENSHIFT_VERSION-4.7.0}" SNC_PRODUCT_NAME=${SNC_PRODUCT_NAME:-crc}
 BASE_DOMAIN=${CRC_BASE_DOMAIN:-testing}
 VM_PREFIX=${SNC_PRODUCT_NAME}-abcde
-VM_IP=192.168.126.11
-
-CRC_ZSTD_EXTRA_FLAGS="--fast"
-
-# Prepare fake directory structure matching create_disk expectations
+VM_IP=192.168.126.11  CRC_ZSTD_EXTRA_FLAGS="--fast" # Prepare fake directory structure matching create_disk expectations
 baseDir="test-metadata-generation"
 mkdir $baseDir
 cd $baseDir
 srcDir=src
 destDir=dest
-
-mkdir -p "$srcDir"
-mkdir -p "$srcDir/auth"
-touch "$srcDir"/auth/kubeconfig
-touch id_ecdsa_crc
-touch "$srcDir"/vmlinuz-0.0.0
-touch "$srcDir"/initramfs-0.0.0.img
-
-echo {} | ${JQ} '.version = "1.2"' \
+mkdir -p "$srcDir" mkdir 
+-p "$srcDir/auth" touch 
+"$srcDir"/auth/kubeconfig touch
+id_ecdsa_crc touch 
+"$srcDir"/vmlinuz-0.0.0 touch 
+"$srcDir"/initramfs-0.0.0.img echo {} | ${JQ} '.version = "1.2"' \
     | ${JQ} '.type = "snc"' \
     | ${JQ} ".buildInfo.buildTime = \"$(date -u --iso-8601=seconds)\"" \
     | ${JQ} ".buildInfo.openshiftInstallerVersion = \"0.0.0\"" \
@@ -39,13 +27,12 @@ echo {} | ${JQ} '.version = "1.2"' \
     | ${JQ} ".clusterInfo.clusterName = \"${SNC_PRODUCT_NAME}\"" \
     | ${JQ} ".clusterInfo.baseDomain = \"${BASE_DOMAIN}\"" \
     | ${JQ} ".clusterInfo.appsDomain = \"apps-${SNC_PRODUCT_NAME}.${BASE_DOMAIN}\"" >${srcDir}/crc-bundle-info.json
-
-download_oc
-
-mkdir -p "$destDir/linux"
-${QEMU_IMG} create -f qcow2 "$destDir/linux/${SNC_PRODUCT_NAME}.qcow2" 64M
-copy_additional_files "$srcDir" "$destDir/linux"
-create_tarball "$destDir/linux"
-
+download_oc 
+mkdir
+-p "$destDir/linux" ${QEMU_IMG}
+create -f qcow2 
+"$destDir/linux/${SNC_PRODUCT_NAME}.qcow2" 64M copy_additional_files 
+"$srcDir" "$destDir/linux" create_tarball "$destDir/linux"
 generate_hyperv_bundle "$destDir/linux" "$destDir/windows"
-generate_hyperkit_bundle "$destDir/linux" "$destDir/macos" "$srcDir" "0.0.0" "init=/init/sh"
+generate_hyperkit_bundle "$destDir/linux" 
+"$destDir/macos" "$srcDir" "0.0.0" "init=/init/sh"
